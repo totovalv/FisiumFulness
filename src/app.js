d7e0d78474e967764345756348b082f5180326b5
Tomas Flores <tomy24ssf@gmail.com>
2023-07-04 17:41:35 -0300

    refactory config cors

--- src/app.js
+++ src/app.js
@@ -1,32 +1,28 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const userRouter = require("./routes/userRoutes");
-const blogRouter = require("./routes/blogRoutes");
-const typeRouter = require("./routes/typeRoutes");
-const commentRouter = require("./routes/commentRoutes");
-const registerRouter = require("./routes/registerRoutes");
-
-
-
-
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/userRoutes');
+const blogRouter = require('./routes/blogRoutes');
+const typeRouter = require('./routes/typeRoutes');
+const commentRouter = require('./routes/commentRoutes');
+const registerRouter = require('./routes/registerRoutes');
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000/" }));
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(bodyParser.json());
-app.use(cors())
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-app.use("/users", userRouter);
-app.use("/blogs", blogRouter);
-app.use("/types", typeRouter);
-app.use("/comments", commentRouter);
-app.use("/register", registerRouter);
+app.use('/users', userRouter);
+app.use('/blogs', blogRouter);
+app.use('/types', typeRouter);
+app.use('/comments', commentRouter);
+app.use('/register', registerRouter);
 
-require("./db");
+require('./db');
 module.exports = app;
