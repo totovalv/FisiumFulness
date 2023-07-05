143185076214ee0fe1818c987fa422aa2f9758e8
Tomas Flores <tomy24ssf@gmail.com>
2023-07-05 12:16:35 -0300

    refactory userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,63 +1,106 @@
-const User = require("../models/User");
+const User = require('../models/User');
 
 exports.newUser = async (req, res) => {
-  const user = new User(req.body);
-
+  const {
+    email,
+    firstname,
+    lastname,
+    status,
+    password,
+    username,
+    role,
+    token,
+    confirm,
+    phone,
+  } = req.body;
+  const newData = {
+    email,
+    firstname,
+    lastname,
+    status,
+    password,
+    username,
+    role,
+    token,
+    confirm,
+    phone,
+  };
   try {
+    const user = new User(newData);
     await user.save();
-    res.status(200).send(user);
+    return res.status(200).json({ user });
   } catch (error) {
-    res.status(400).send(error.message);
+    return res.status(400).json({ message: error.message });
   }
 };
 exports.getUser = async (req, res) => {
   const { email } = req.query;
   try {
-    const user = await User.find({});
-    if (email) {
-      const userFilter = user.filter((user) =>
-        user.email.toLowerCase().includes(email.toLowerCase())
-      );
-      if (userFilter.length) {
-        return res.status(200).send(userFilter);
-      } else {
-        res.status(404).send(error.message);
-      }
-    } else {
-      res.status(200).send(user);
-    }
+    const users = await User.find({});
+    if (!email) return res.status(200).json({ users });
+
+    const userFilter = users.filter((user) =>
+      user.email.toLowerCase().includes(email.toLowerCase())
+    );
+    if (!userFilter.length) throw new Error('user not found');
+
+    return res.status(200).json({ userFilter });
   } catch (error) {
-    res.status(404).send(error.message);
+    return res.status(404).json({ message: error.message });
   }
 };
 exports.updateUser = async (req, res) => {
+  const id = req.params.id;
+  const {
+    email,
+    firstname,
+    lastname,
+    status,
+    password,
+    username,
+    role,
+    token,
+    confirm,
+    phone,
+  } = req.body;
+  const newData = {
+    email,
+    firstname,
+    lastname,
+    status,
+    password,
+    username,
+    role,
+    token,
+    confirm,
+    phone,
+  };
   try {
-    await User.findByIdAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-    });
-    res.status(200).json("User has been updated");
+    await User.findByIdAndUpdate({ _id: id }, newData);
+    return res.status(200).json({ message: 'User has been updated' });
   } catch (error) {
-    res.status(400).send(error.message);
+    return res.status(400).json({ message: error.message });
   }
 };
 exports.deleteUser = async (req, res) => {
+  const id = req.params.id;
   try {
-    await User.findOneAndRemove({ _id: req.params.id });
+    await User.findOneAndRemove({ _id: id });
 
-    res.status(200).json("User has been deleted");
+    return res.status(200).json({ message: 'User has been deleted' });
   } catch (error) {
-    res.status(400).send(error.message);
+    return res.status(400).json({ message: error.message });
   }
 };
 exports.getDetail = async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const id = req.params.id;
 
   try {
-    if (!user) {
-      throw new Error("User not found");
-    }
-    res.status(200).json(user);
+    const user = await User.findById(id);
+    if (!user) throw new Error('User not found');
+
+    return res.status(200).json({ user });
   } catch (error) {
-    res.status(400).send(error.message);
+    return res.status(400).json({ message: error.message });
   }
 };
